2c8184afc20505b9ca6c09cbc149abef18ff4f78
pauldes <paul.desigaud@wavestone.com>
2021-10-29 19:27:41 +0200

    Ajout d'une description du fichier streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+"""Ce fichier sert au déploiement sur streamlit.io. Il permet de lancer
+l'application Web depuis la racine du projet avec le CLI Streamlit (streamlit run)
+"""
+
 from qualite_decp import web
 
 web.app.run()
