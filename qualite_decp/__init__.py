2f588a7a592669d41bdff2ee69b8d2f2caf2e72f
pauldes <paul.desigaud@wavestone.com>
2021-10-29 15:07:38 +0200

    Ajout d'une documentation

--- qualite_decp/__init__.py
+++ qualite_decp/__init__.py
@@ -1,6 +1,8 @@
 import yaml
 import munch
 
+# Charge les entrées du fichier de configuration et les 
+# place dans un objet python nommé 'conf' utilisé dans le reste du projet
 with open('qualite_decp/conf.yaml', encoding="utf-8") as f:
     conf_dict = yaml.safe_load(f)
 conf = munch.DefaultMunch.fromDict(conf_dict)
