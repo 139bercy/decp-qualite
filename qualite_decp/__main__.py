16debf88875934978555901aac575fdba010f6a7
pauldes <paul.desigaud@wavestone.com>
2021-10-29 15:25:13 +0200

    Restructuration du CLI

--- qualite_decp/__main__.py
+++ qualite_decp/__main__.py
@@ -1,20 +1,4 @@
-import argparse
-
-def get_parser():
-    """
-    Creates a new argument parser.
-    """
-    parser = argparse.ArgumentParser()
-    return parser
-
-def main(args=None):
-    """ Main entry point.
-    
-    Args:
-        args : list of arguments as if they were input in the command line.
-    """
-    parser = get_parser()
-    args = parser.parse_args(args)
+import qualite_decp.cli
 
 if __name__ == '__main__':
-    main()
+    qualite_decp.cli.run()
