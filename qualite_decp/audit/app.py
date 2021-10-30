5044ca0ed35a1a18eb6268167731900ab6d6b932
pauldes <paul.desigaud@wavestone.com>
2021-10-30 17:56:16 +0200

    Filtrage et préparation de l'audit

--- qualite_decp/audit/app.py
+++ qualite_decp/audit/app.py
@@ -3,6 +3,9 @@
 
 import logging
 
+from qualite_decp import download
+from qualite_decp import conf
+
 
 def run(rows: int = None):
     """Audite la donnée consolidée et stocke les résultats.
@@ -10,4 +13,31 @@ def run(rows: int = None):
     Args:
         rows (int, optional): Nombre de lignes desquelles auditer la qualité. Defaults to None.
     """
-    pass
+    data = download.open_json(conf.download.consolidated_data_path)
+    schema = download.open_json(conf.download.consolidated_data_schema_path)
+    # Choix d'un sous-ensemble des marchés, si requis
+    if rows is not None:
+        data["marches"] = data["marches"][:rows]
+    # Filtrage sur le _type 'marché'
+    num_total = len(data["marches"])
+    availables_types = set([m.get("_type") for m in data["marches"]])
+    logging.debug("Valeurs de la colonne _type : %s", availables_types)
+    logging.debug("Filtrage de la colonne _type sur la valeur 'marché'")
+    data["marches"] = [
+        m for m in data["marches"] if m.get("_type").lower() == "marché".lower()
+    ]
+    num_filtered = len(data["marches"])
+    logging.debug(
+        "Passage de %d à %d entrées suite au filtrage", num_total, num_filtered
+    )
+    # Audit par source
+    available_sources = set([m.get("source") for m in data["marches"]])
+    for source in conf.audit.sources:
+        source_data = {
+            "marches": [
+                m for m in data["marches"] if m.get("source").lower() == source.lower()
+            ]
+        }
+        logging.debug(
+            "%d lignes pour la source %s", len(source_data["marches"]), source
+        )
