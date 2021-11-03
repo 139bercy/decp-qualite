84210927d54863af5242f182eeb77ba5d7ffba49
pauldes <paul.desigaud@wavestone.com>
2021-11-03 16:27:27 +0100

    Chargement des artifacts

--- qualite_decp/web/app.py
+++ qualite_decp/web/app.py
@@ -1,44 +1,29 @@
 import logging
+import datetime
 
 import streamlit as st
 
 from qualite_decp import conf
 from qualite_decp import download
 from qualite_decp.web import build
+from qualite_decp.web import artifacts
 from qualite_decp.audit import audit_results
 from qualite_decp.audit import audit_results_one_source
-
-
-def get_audit_results(
-    date: str, source: str
-) -> audit_results_one_source.AuditResultsOneSource:
-    """Récupère le résultat de l'audit de qualité pour la source et la date selectionnées.
-
-    Args:
-        date (str): Date de l'audit voulu
-        source (str): Source auditée
-
-    Returns:
-        audit_results_one_source.AuditResultsOneSource: Résultats d'audit
-    """
-    # TODO
-    if date == "Exemple date 1":
-        path = "./data/audit.json"
-    elif date == "Exemple date 2":
-        path = "./data/audit_old.json"
-    results = audit_results.AuditResults.from_json(path)
-    result = results.extract_results_for_source(source)
-    return result
 
 
 def run():
     """Lance l'application web de présentation des résultats"""
     build.page_config()
+    artifacts_dict = artifacts.list_artifacts()
     available_sources = conf.audit.sources
-    available_dates = ["Exemple date 1", "Exemple date 2"]
-    selected_source, current_date, old_date = build.sidebar(
+    available_dates = artifacts_dict.keys()
+    selected_source, selected_current_date, selected_comparison_date = build.sidebar(
         available_sources, available_dates
     )
-    current_results = get_audit_results(current_date, selected_source)
-    old_results = get_audit_results(old_date, selected_source)
-    build.page(current_results, old_results)
+    current_results = artifacts.get_audit_results(
+        selected_current_date, selected_source
+    )
+    comparison_results = artifacts.get_audit_results(
+        selected_comparison_date, selected_source
+    )
+    build.page(current_results, comparison_results)
