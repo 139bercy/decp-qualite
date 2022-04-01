1566ffae44da08c03fad79f70f449232fb108ec2
pauldes <paul.desigaud@wavestone.com>
2022-04-01 12:01:45 +0200

    Nom des modules explicites

--- decp_qualite/setup.py
+++ decp_qualite/setup.py
@@ -4,4 +4,9 @@ setup(
     name="decp_qualite",
     version="0.1",
     description="Evaluation de la qualité des Données Essentielles de la Commande Publique (DECP).",
+    py_modules=[
+        "audit",
+        "download",
+        "web",
+    ],
 )
